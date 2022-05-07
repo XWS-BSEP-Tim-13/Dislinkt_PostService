a9e12e7739a724dd65ad38a9e7200928d2c99997
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2022-05-07 21:20:25 +0200

    feat: conflicts resolved

--- application/post_service.go
+++ application/post_service.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"errors"
 	"fmt"
 	"github.com/XWS-BSEP-Tim-13/Dislinkt_PostService/domain"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -56,4 +57,20 @@ func (service *PostService) ReactToPost(reaction *domain.Reaction) (string, erro
 	}
 
 	return postID, nil
+}
+
+func (service *PostService) CreateNewPost(post *domain.Post) (*domain.Post, error) {
+	//dbUser, _ := service.store.GetByUsername((*user).Username)
+	//if dbUser == nil {
+	//	err := errors.New("user with this username not exists")
+	//	return nil, err
+	//}
+	(*post).Id = primitive.NewObjectID()
+	err := service.store.Insert(post)
+	if err != nil {
+		err := errors.New("error while creating new post")
+		return nil, err
+	}
+
+	return post, nil
 }
